edbb49134620733b452fd4c05911c8a1c34d8d06
DripSwag <mimirani33@gmail.com>
2022-12-29 10:08:40 +0000

    Added cookie functionality to just the Music

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import trance from './assets/Y2Mate.is - Trance - 009 Sound System Dreamscape (HD)-TKfS5zVfGBc-128k-1657378497730.mp3'
 import SoundEffectPlayer from './components/soundEffectPlayer.jsx'
 import AudioPlayer from './components/audioPlayer.jsx';
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Cookies, CookiesProvider, useCookies } from 'react-cookie';
 import './App.css';
 
 function App() {
   let [cookieDictionary, setCookieDictionary] = useState({})
+  const [cookies, setCookie] = useCookies()
 
   //This is messy but it works so im not touching this
   const changeDictionary = (key, value) => {
@@ -23,16 +25,28 @@ function App() {
     }
   }
 
+  //useCallback used because its just a code thing. It would infinetly call in useEffect because it has no visual thing to render and its the same visual thing all the time. useCallback does like this cache thing and only calls if the new data is different to cache
+  const makeCookie = useCallback(() => {
+    for(var key in cookieDictionary){
+      setCookie(key, String(cookieDictionary[key]))
+    }
+  }, [cookieDictionary])
+
+  useEffect(() => {
+    makeCookie()
+  }, [makeCookie])
+
   const showCookie = () =>{
     console.log(cookieDictionary)
+    console.log(cookies.Fire, cookies.Audio)
   }
 
   return (
-    <div>
+    <CookiesProvider>
       <SoundEffectPlayer sound={trance} name={"Fire"} changeDictionary={changeDictionary}/>
-      <AudioPlayer changeDictionary={changeDictionary}/>
+      <AudioPlayer changeDictionary={changeDictionary} cookies={cookies}/>
       <button onClick={showCookie}>Show Cookie</button>
-    </div>
+    </CookiesProvider>
   );
 }
 export default App;
--- src/components/audioPlayer.jsx
+++ src/components/audioPlayer.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player/youtube';
 
-function AudioPlayer({ changeDictionary }){
+function AudioPlayer({ changeDictionary, cookies }){
   let [playingAudio, setPlayingAudio] = useState(false)
-  let [audioVolume, setAudioVolume] = useState()
+  let [audioVolume, setAudioVolume] = useState(() => {
+    if (cookies.Audio != undefined){
+      return cookies.Audio
+    }
+    else{
+      return null
+    }
+  })
 
   const playAudio = () => {
     setPlayingAudio(!playingAudio)
@@ -12,14 +19,13 @@ function AudioPlayer({ changeDictionary }){
   //I have to call setAudioVolume twice because the onChange isnt on render, its after render. The first time the slider is clicked, nothing is registered because the input is not saved until after the render.
   useEffect(() => {
     setAudioVolume(audioVolume)
-    console.log(audioVolume)
     changeDictionary("Audio", audioVolume)
   }, [setAudioVolume, audioVolume])
 
   return (
     <div>
       <button onClick={playAudio}>{playingAudio ? "Pause" : "Play"}</button>
-      <input type="range" max='100' min='0' onChange={(event) => setAudioVolume(parseFloat(event.target.value) / 100)}></input>
+      <input type="range" max='100' min='0' onChange={(event) => setAudioVolume(parseFloat(event.target.value) / 100)} value={String(audioVolume * 100)}></input>
       <ReactPlayer url='https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl' playing={playingAudio} width='0' height="0" volume={audioVolume} loop={true} />
     </div>
   ); 
